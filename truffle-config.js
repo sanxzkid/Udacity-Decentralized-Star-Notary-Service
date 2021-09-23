dd4c594293b0dd696f17889dff1429d0dd83e1e3
Felipe Sanchez <f.sanxz@gmail.com>
2021-09-23 10:27:25 -0300

    last commit

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,12 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = "INSERT infuraKey";
+//
+// const fs = require('fs');
+const mnemonic = "INSERT mnemonic wallet";
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -61,6 +67,17 @@ module.exports = {
       // from: <address>,        // Account to send txs from (default: accounts[0])
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
+
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `wss://rinkeby.infura.io/ws/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
